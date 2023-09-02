4575341f3351adcf084244efdab7fe03f7505d94
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-09-02 14:54:46 +0200

    admin page

---
+++ frontend/src/components/pages/AdminPage.jsx
@@ -0,0 +1,26 @@
+function AdminPage() {
+    return (
+     <ul className="admin-page">
+        <li className="admin-page_title">
+            <h2>Créer votre personnage</h2>
+        </li>
+        <li className="admin-page_form">
+            <form>
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <button type="submit" onClick={handleSubmit}>Submit</button>
+            </form>
+        </li>
+          <li className="admin-page_message_success">
+            <div className="progress-bar"></div>
+            <p>Personnage crée !</p>
+          </li>
+     </ul>
+    )
+}
+
+export default AdminPage;
