bf8388ab449508cccc7fe536f6bf6c785d71478d
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-09-13 11:35:15 +0200

    upload avec multer OK

--- backend/src/routes/upload.route.js
+++ backend/src/routes/upload.route.js
@@ -1,11 +1,10 @@
 import express from "express";
 import multer from "multer"; // pour rendre dispo le type FormData.
+import uploadController from "../controllers/upload.controller.js"
 
 const router = express.Router();
 const upload = multer({dest: "./public/tmp"});
 
-router.post("/", multer.single("image"), function (req, res, next){
-    console.log(req.file);
-})
+router.post("/", upload.single("image"), uploadController)
 
 export default router;
